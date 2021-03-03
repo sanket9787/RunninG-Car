62b0b89dede54630937ddca749c6e434bd095289
sanket9787 <57309022+sanket9787@users.noreply.github.com>
2021-03-03 20:24:24 +0530

    SUmmary

---
+++ magic.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $surface=$('.surface');
+    $car=$('.car');
+ $img=$('.car img');
+ let flag=true;
+
+const cars = ['./assets/Img_06.png','./assets/Img_05.png']
+
+$(document).on('keypress',function(e){
+if(e.which==13){
+    $($surface).toggleClass('moveright');
+    $($car).toggleClass('suspenstion'); 
+}
+})
+
+
+$(document).on('keypress',function(e){
+    if(e.which==119){
+        if(flag){
+            flag=false;
+           $img.attr('src',cars[0]);
+        }
+        else{
+            flag=true;
+            $img.attr('src',cars[1]);
+        }
+    }
+    })
+
+});
+
